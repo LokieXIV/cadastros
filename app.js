b5588c3aab4f448295d58caf146805fe03623dae
unknown <rafafr13@hotmail.com>
2023-12-06 09:20:43 -0300

    adicionando um console.log

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 console.log("Rodando sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 console.log("Projeto Galdino")
+//Comentario
+console.log("aplicação alura")
+console.log("enviando para o github")
